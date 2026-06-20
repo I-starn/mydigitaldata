29bbc82cd0c5d655fd839662abe88f90a549b791
I-Starn <isaacstanfordbanda@gmail.com>
2026-06-19 23:14:07 -0800

    color

--- src/components/NeonBackground.tsx
+++ src/components/NeonBackground.tsx
@@ -1,12 +1,13 @@
+// src/components/NeonBackground.tsx
 'use client';
 import { motion } from 'framer-motion';
 
 export default function NeonBackground() {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden bg-darkBg">
-      {/* Cyan Blob */}
+      {/* 1. Signature Brand Orange Blob (Bottom of logo) */}
       <motion.div
-        className="absolute w-[300px] h-[300px] rounded-full bg-neonCyan opacity-20 blur-[100px]"
+        className="absolute w-[300px] h-[300px] rounded-full bg-brandOrange opacity-[0.14] blur-[110px]"
         animate={{
           x: [0, 100, -50, 0],
           y: [0, -80, 50, 0],
@@ -18,9 +19,10 @@ export default function NeonBackground() {
         }}
         style={{ top: '10%', left: '15%' }}
       />
-      {/* Pink Blob */}
+      
+      {/* 2. Warm Automotive Amber-Gold Blob */}
       <motion.div
-        className="absolute w-[350px] h-[350px] rounded-full bg-neonPink opacity-[0.15] blur-[110px]"
+        className="absolute w-[350px] h-[350px] rounded-full bg-brandAmber opacity-[0.12] blur-[120px]"
         animate={{
           x: [0, -120, 80, 0],
           y: [0, 100, -80, 0],
@@ -32,9 +34,10 @@ export default function NeonBackground() {
         }}
         style={{ bottom: '15%', right: '10%' }}
       />
-      {/* Purple Blob */}
+      
+      {/* 3. Deep Brand Rust Shading Blob */}
       <motion.div
-        className="absolute w-[250px] h-[250px] rounded-full bg-neonPurple opacity-20 blur-[90px]"
+        className="absolute w-[250px] h-[250px] rounded-full bg-brandRust opacity-[0.15] blur-[100px]"
         animate={{
           x: [0, 50, -60, 0],
           y: [0, 60, -40, 0],
